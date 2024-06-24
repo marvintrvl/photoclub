b9f21da991b53cc0969d028fe194d5f7ed9e0a1b
Marvin Menzel <93315675+marvintrvl@users.noreply.github.com>
2024-06-24 13:57:29 +0200

    styling

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from members.models import CustomUser
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    newest_users = CustomUser.objects.order_by('-date_joined')[:3]
+    return render(request, 'main/index.html', {'newest_users': newest_users})
