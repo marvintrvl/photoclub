ab02692c9e24a9749a03e009524163e0563c6b63
Marvin Menzel <93315675+marvintrvl@users.noreply.github.com>
2024-06-27 16:37:31 +0200

    before profile change

--- main/views.py
+++ main/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from members.models import CustomUser
 from meetups.models import Meetup
+from photo_challenge.models import PhotoChallenge
+from editing_challenge.models import EditingChallenge
 from django.utils import timezone
 
 def index(request):
     newest_users = CustomUser.objects.order_by('-date_joined')[:3]
     upcoming_meetup = Meetup.objects.filter(date__gte=timezone.now()).order_by('date').first()
-    return render(request, 'main/index.html', {'newest_users': newest_users, 'upcoming_meetup': upcoming_meetup})
+    nearest_photo_challenge = PhotoChallenge.objects.filter(end_date__gte=timezone.now()).order_by('end_date').first()
+    nearest_editing_challenge = EditingChallenge.objects.filter(end_date__gte=timezone.now()).order_by('end_date').first()
+    return render(request, 'main/index.html', {
+        'newest_users': newest_users,
+        'upcoming_meetup': upcoming_meetup,
+        'nearest_photo_challenge': nearest_photo_challenge,
+        'nearest_editing_challenge': nearest_editing_challenge
+    })
